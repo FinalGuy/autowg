b5a67daa231243e168714012fb4c58426c481a26
tfojuth <torben.fojuth@posteo.de>
2016-08-17 22:24:41 +0200

    Formular für Kilometerstandeingabe gebaut

--- src/autowg/routes/home.clj
+++ src/autowg/routes/home.clj
@@ -2,20 +2,38 @@
   (:require [compojure.core :refer :all]
             [autowg.views.layout :as layout]))
 
-;<div class="jumbotron">
-;<h1>Jumbotron heading</h1>
-;<p class="lead"> sit amet risus.</p>
-;<p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
-;</div>
+
+(defn form []
+  [:form {:role "form" :class "form-horizontal"}
+   [:div {:class "form-group"}
+    [:label {:for "stand" :class "control-label col-sm-2"} "Kilometerstand:"]
+    [:div {:class "col-sm-10"}
+     [:input {:type "number" :class "form-control" :id "stand"}]]]
+   [:div {:class "form-group"}
+    [:label {:for "fahrer" :class "control-label col-sm-2"} "Fahrer:"]
+    [:div {:class "col-sm-10"}
+     [:select {:class "form-control" :id "fahrer"}
+      [:option "Sonja & Torben"]
+      [:option "Jessica & Nils"]
+      ]]]
+   [:div {:class "form-group"}
+    [:div {:class "col-sm-offset-2 col-sm-10"}
+     [:button {:type "submit" :class "btn btn-success"} "eintragen"]]]])
+
 
 (defn home []
   (layout/common
     [:div {:class "container"}
-     [:div {:class "header clearfix"}
-      [:div {:class "jumbotron"}
+     [:div {:class "jumbotron"}
+      [:div {:class "container"}
        [:h1 "Hallo"]
-       [:p {:class "lead"} "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo"]
-       ]]]))
+       [:p {:class "lead"} "Hier können wir die Kilometer aufschreiben"]]]
+     [:div {:class "panel panel-success"}
+      [:div {:class "panel-heading"}
+       [:h3 "Kilometerstand eintragen"]]
+      [:div {:class "panel-body"}
+       (form)]]
+     ]))
 
 (defroutes home-routes
            (GET "/" [] (home)))
