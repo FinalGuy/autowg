58d80a173b307b457b08f7b9cd7ece44beaa923a
tfojuth <torben.fojuth@posteo.de>
2016-08-18 09:42:38 +0200

    Autoformatter

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
                  [ring-server "0.3.1"]]
   :plugins [[lein-ring "0.8.12"]]
   :ring {:handler autowg.handler/app
-         :init autowg.handler/init
+         :init    autowg.handler/init
          :destroy autowg.handler/destroy}
   :profiles
   {:uberjar {:aot :all}
    :production
-   {:ring
-    {:open-browser? false, :stacktraces? false, :auto-reload? false}}
+            {:ring
+             {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.1"]]}})
+            {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.3.1"]]}})
