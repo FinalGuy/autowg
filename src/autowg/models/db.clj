b03e35c887866f01674a09907652c145d3b1c2f2
tfojuth <torben.fojuth@posteo.de>
2016-09-19 20:31:03 +0200

    Fixed initialisation of the "trips" table.

--- src/autowg/models/db.clj
+++ src/autowg/models/db.clj
@@ -7,13 +7,17 @@
          :user     "clojure"
          :password "clojure"})
 
-(defn create-trips-table []
-  (jdbc/db-do-commands db (jdbc/create-table-ddl :trips
-                                                 [[:id :serial :primary :key]
-                                                  [:driver :text]
-                                                  [:mileage :integer]
-                                                  [:timestamp :timestamp :default :current_timestamp]])))
 
+
+(defn create-trips-table []
+  (def result (jdbc/query db ["SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'trips');"]))
+  (if (get (first result) :exists)
+    (println "Tabelle existiert bereits")
+    (jdbc/db-do-commands db (jdbc/create-table-ddl :trips
+                                                   [[:id :serial :primary :key]
+                                                    [:driver :text]
+                                                    [:mileage :integer]
+                                                    [:timestamp :timestamp :default :current_timestamp]]))))
 
 (defn save-trip [driver mileage]
   (jdbc/insert! db :trips {:driver driver :mileage (read-string mileage)}))
