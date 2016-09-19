98f3d492c08dd210561d397fef5e27197956599f
tfojuth <torben.fojuth@posteo.de>
2016-09-19 21:04:29 +0200

    Just chose a background color for the page.

--- src/autowg/views/layout.clj
+++ src/autowg/views/layout.clj
@@ -11,6 +11,6 @@
      [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" :integrity "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" :crossorigin "anonymous"}]
      [:script {:src "https://code.jquery.com/jquery-3.1.0.slim.min.js" :integrity "sha256-cRpWjoSOw5KcyIOaZNo4i6fZ9tKPhYYb6i5T9RSVJG8=", :crossorigin :anonymous }]
      [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" :integrity "sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" :crossorigin "anonymous"}]
-     ;(include-css "/css/screen.css")
+     (include-css "/css/screen.css")
      ]
     [:body body]))
